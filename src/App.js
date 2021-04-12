4a41cbf066d2eb6c7fd271442445a76f92706ada
dgeo-dev <dgeodev@gmail.com>
2021-04-12 19:50:07 +0200

    modif method 8

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import ContactSuccess from "./pages/contactSuccess"
 function App() {
   return (
