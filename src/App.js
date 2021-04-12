e251fec922f95ada53d7a8e4a29912f13f97f691
dgeo-dev <dgeodev@gmail.com>
2021-04-12 20:25:36 +0200

    test redirect 2

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Route path="/">
             <form
               name="contact"
+              action="/pages/contactSuccess"
               data-netlify="true"
               method="post"
               onSubmit="submit"
